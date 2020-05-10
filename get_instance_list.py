51c63f978ef9b415fce9202b434aa08e3918a462
David Hong <secuof@Davids-iMac.local>
2020-05-10 22:21:33 +0900

    update

--- get_instance_list.py
+++ get_instance_list.py
@@ -1,22 +1,19 @@
-import boto3    
+import boto3
 import pprint
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
+# instancetype = ec2client.describe_instance_types()
+# region = ec2client.describe_regions()
 pp = pprint.PrettyPrinter(width=20, indent=4)
 
 for reservation in response["Reservations"]:
-    print(reservation)
     for instance in reservation["Instances"]:
-        # pp.pprint(instance)
-        instance_id = instance["InstanceId"]
-        instance_type = instance["InstanceType"]
-        instance_pri_addr = instance["PrivateIpAddress"]
-        instance_tags = instance["Tags"]
-        print(instance_tags[0]["Value"])
-        # instance_pub_addr = instance["PublicIpAddress"]
-        print(instance_id)
-        print(instance_type)
-        print(instance_pri_addr)
-        # print(instance_tags)
-        # print(instance_pub_addr)
-        # pp.pprint(instance_id + instance_type, + instance_pri_addr + instance_pub_addr)
+        pp.pprint(instance["InstanceId"])
+        netlist = instance["NetworkInterfaces"]
+        for net in netlist:
+            if "Association" in net:
+                pp.pprint((net["Association"])["PublicIp"])
+            else:
+                pp.pprint(net["PrivateIpAddress"])
+            # pp.pprint(net["PrivateIpAddresses"][0])
+        # pp.pprint(instance["Tags"]) ## Tags is List
